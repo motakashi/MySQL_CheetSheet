356d14f6101720a8fa32c57674ba6ea48bbf4de8
motakashi <takashi_home_sky@yahoo.co.jp>
2020-06-06 23:02:20 +0900

    Update myapp.sql

--- myapp.sql
+++ myapp.sql
@@ -1,3 +1,26 @@
+/*
+number:
+- int
+- float
+- double
+- int unsigned
+
+string:
+- char(4)
+- varchar(255)
+- text
+
+date/time:
+- date
+- time
+- datetime '2017-07-22 17:22:33'
+
+true/false:
+- boolean -> tinyint(1)
+  true -> 1
+  false -> 0
+*/
+
 drop table if exists users;
 create table users (
   id int unsigned,

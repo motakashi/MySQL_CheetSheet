3f56c7a5c865a51f563223c74fc27286d6164734
motakashi <takashi_home_sky@yahoo.co.jp>
2020-06-06 23:06:44 +0900

    Update myapp.sql

--- myapp.sql
+++ myapp.sql
@@ -27,3 +27,15 @@ create table users (
   name varchar(20),
   score float
 );
+
+/*
+insert into users (id, name, score) values (1, 'taguchi', 5.8);
+insert into users (id, name, score) values (2, 'fkoji', 8.2);
+insert into users (id, name, score) values (3, 'dotinstall', 6.1);
+insert into users (id, name, score) values (4, 'yamada', null);
+*/
+insert into users (id, name, score) values
+  (1, 'taguchi', 5.8),
+  (2, 'fkoji', 8.2),
+  (3, 'dotinstall', 6.1),
+  (4, 'yamada', null);
